55c5cf4c7e8328cd9c543e1431bea2126a9c2ea0
aylongo <aylongo10@gmail.com>
2023-08-11 12:03:30 +0300

    fix(): data and columns are in hebrew

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -3,15 +3,12 @@ import { EditableTable } from "../EditableTable";
 import { columns } from "./ColumnDefinition";
 
 const rows = [
-  { id: 1, lastName: "You", firstName: "Fuck", age: 35, selection: "BR", customField: 'שמן' },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 1, lastName: "מצב", firstName: "מה", age: 35, country: "BR", weight: 'שמן' },
+  { id: 2, lastName: "הנמר", firstName: "אפי", age: 42 },
+  { id: 3, lastName: "אלעד", firstName: "אדיר", age: 45 },
+  { id: 4, lastName: "מאט", firstName: "שאזא", age: 16 },
+  { id: 5, lastName: "דודי", firstName: "דודי", age: null, isRolling: true },
+  { id: 6, lastName: "ללא תקנה", firstName: null, age: 150 },
 ];
 
 const Wrapper = () => {
