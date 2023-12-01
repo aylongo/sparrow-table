a5c13947e5b9a9da428b72cb5e5efc5d2c6bba52
aylongo <aylongo10@gmail.com>
2023-12-01 15:32:23 +0200

    fix(): changed scrollbar style

--- src/components/Table/StyledBox.tsx
+++ src/components/Table/StyledBox.tsx
@@ -1,6 +1,17 @@
 import { Box, styled } from "@mui/material";
 
 const StyledBox = styled(Box)(({ theme }) => ({
+  "*::-webkit-scrollbar": {
+    width: "0.5em",
+    height: "0.5em"
+  },
+  "*::-webkit-scrollbar-track": {
+    "-webkit-box-shadow": "inset 0 0 6px rgba(144, 144, 144, 0.25)",
+  },
+  "*::-webkit-scrollbar-thumb": {
+    backgroundColor: "rgba(150, 144, 144, 0.5)",
+    borderRadius: 8
+  },
   "& .MuiDataGrid-cell--editing": {
     backgroundColor: "rgb(255,215,115, 0.19)",
     color: "#1a3e72",
@@ -12,7 +23,7 @@ const StyledBox = styled(Box)(({ theme }) => ({
     backgroundColor: `rgb(204,0,0, ${theme.palette.mode === "dark" ? 0.2 : 0})`,
     color: theme.palette.error.main,
   },
-  direction: 'rtl'
+  direction: "rtl",
 }));
 
 export default StyledBox;
