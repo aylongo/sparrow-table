19e8c04267406c6abf750aa219974d07c25e2234
aylongo <aylongo10@gmail.com>
2024-01-29 00:42:02 +0200

    Updates

---
+++ precache-manifest.c700cb2dbbd49038b7170064a156d306.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "afab9e2fa42731e9ebf5",
+    "url": "/sparrow-table/static/css/main.003073df.chunk.css"
+  },
+  {
+    "revision": "afab9e2fa42731e9ebf5",
+    "url": "/sparrow-table/static/js/main.afab9e2f.chunk.js"
+  },
+  {
+    "revision": "72fad8d9e79c1d9d2794",
+    "url": "/sparrow-table/static/js/1.72fad8d9.chunk.js"
+  },
+  {
+    "revision": "f52a499412d14a04f604",
+    "url": "/sparrow-table/static/js/2.f52a4994.chunk.js"
+  },
+  {
+    "revision": "3ec9c96a3a976a0a0eb2",
+    "url": "/sparrow-table/static/js/runtime~main.3ec9c96a.js"
+  },
+  {
+    "revision": "c801a2aca7803bb7ef80101e6ee93a43",
+    "url": "/sparrow-table/index.html"
+  }
+];
